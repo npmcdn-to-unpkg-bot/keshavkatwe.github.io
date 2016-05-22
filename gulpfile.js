48afb05d238b20f6b5050fca63bf80077fe758ec
Keshav Katwe <keshav.katwe@gmail.com>
2016-05-22 23:08:53 +0530

    home first phase

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,13 @@ var browserSync = require('browser-sync').create();
 
 
 gulp.task('build', shell.task(['jekyll build --watch']));
+gulp.task('jekyll', shell.task(['jekyll serve']));
 
 gulp.task('serve', function () {
     browserSync.init({server: {baseDir: '_site/'}, notify: false});
     gulp.watch('_site/**/*.*').on('change', browserSync.reload);
 });
+
+
 
 gulp.task('default', ['build', 'serve']);
