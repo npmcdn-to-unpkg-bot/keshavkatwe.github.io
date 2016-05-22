83d9c09e8c30fc048f334c6bae7fba5621ca6930
Keshav Katwe <keshav.katwe@gmail.com>
2016-05-22 13:53:19 +0530

    Header complete

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var browserSync = require('browser-sync').create();
 gulp.task('build', shell.task(['jekyll build --watch']));
 
 gulp.task('serve', function () {
-    browserSync.init({server: {baseDir: '_site/'}});
+    browserSync.init({server: {baseDir: '_site/'}, notify: false});
     gulp.watch('_site/**/*.*').on('change', browserSync.reload);
 });
 
